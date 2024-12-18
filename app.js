54e8991d44ef418fd97ed5c3a71022acee67c13a
seonyeonghun2 <kjca1117@naver.com>
2024-12-18 16:06:51 +0900

    서버 설정 | 미들웨어 장착, 라우터 연결

---
+++ app.js
@@ -0,0 +1,18 @@
+//const express = require('express')
+import express from 'express';
+import indexRouter from './routes/index.js';
+import userRouter from './routes/user.js';
+import db from './models/index.js';
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public')); // static
+app.use(express.json()); // json 파싱
+app.use(express.urlencoded({ extended: true })); // form 데이터 파싱
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}...`);
+});
