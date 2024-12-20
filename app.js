4e0c10661863ef2ea58fda394dc7f8854d700467
seonyeonghun2 <kjca1117@naver.com>
2024-12-20 16:12:07 +0900

    템플릿 문서 | Nunjucks 뷰 엔진 설정

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-//const express = require('express')
 import express from 'express';
+import nunjucks from 'nunjucks';
 import indexRouter from './routes/index.js';
 import userRouter from './routes/user.js';
 import db from './models/index.js';
 const app = express();
+
+app.set("view engine", "html");
+
+nunjucks.configure("views", {
+  autoescape: true,
+  express: app,
+  watch: true
+})
 const port = process.env.PORT || 3000;
 
 // Model Synchronization : 모델-DB 동기화
