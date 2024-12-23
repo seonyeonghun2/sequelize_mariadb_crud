c37b2d08e17ca3ca6c5241e7101ecbf9fe810b3c
seonyeonghun2 <kjca1117@naver.com>
2024-12-23 17:07:29 +0900

    라우터 | indexRouter 사용자 목록 역순 조회 및 응답

--- controller/index.js
+++ controller/index.js
@@ -1,9 +1,19 @@
-const getHome = (req, res) => {
-  //   res.send('안녕하세요!');
-  res.render('index.html', {
-    title: '웹개발 실습',
-    subtitle: '사용자 등록 폼',
-  });
+import User from '../models/User.js';
+const getHome = async (req, res) => {
+  try {
+    //  현재 등록된 user 데이터를 수신
+    const users = await User.findAll({
+      order: [['id', 'DESC']],
+    });
+    console.log(users);
+    res.render('index.html', {
+      title: '웹개발 실습',
+      subtitle: '사용자 등록 폼',
+      users,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default { getHome };
