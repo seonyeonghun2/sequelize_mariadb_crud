2fbef5cd502fd91f9a31eb0dbebee7ff295549af
seonyeonghun2 <kjca1117@naver.com>
2024-12-23 17:06:20 +0900

    컨트롤러 | getUsers 요청 핸들러, 역순정렬 응답 추가

--- controller/user.js
+++ controller/user.js
@@ -1,4 +1,4 @@
-import User from "../models/User.js"
+import User from '../models/User.js';
 /**
  * 사용자 등록
  * POST /users
@@ -8,14 +8,24 @@ const addUser = async (req, res) => {
   try {
     const { name, age, married } = req.body;
     const user = await User.create({
-        name,
-        age,
-        married
-    })
+      name,
+      age,
+      married,
+    });
     res.status(201).json(user);
   } catch (err) {
     console.log(err);
   }
 };
 
-export default { addUser }
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll({
+      order: [['id', 'DESC']],
+    });
+    res.status(201).json(users);
+  } catch (err) {
+    console.log(err);
+  }
+};
+export default { addUser, getUsers };
